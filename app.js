3b393c94d25d67d5a850209a9a5dfcbdcbad3ddf
starhelios <starhelios94@gmail.com>
2022-06-14 22:21:52 -0400

    chore: open cors again

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.set("views", path.join(__dirname, "views"));
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
